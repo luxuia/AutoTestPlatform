5f25cf5a09c1ebf84b4baecb2209e9129224c73d
peixy0 <pei.xy0@outlook.com>
2018-03-22 17:05:03 +0800

    Python

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 # -*- coding: utf-8 -*-
 
-import win32gui
-import win32api
-import win32ui
-import win32con
-import ctypes
-
-import multiprocessing, time
-
 from config.work import config
-
-import numpy as np
+import ctypes
 import cv2
+import multiprocessing
+import numpy as np
+import time
+import win32api
+import win32con
+import win32gui
+import win32ui
 
 TITLE_BAR_W = 2
 TITLE_BAR_H = 22
 
-def EnumWindows():
-    def _EnumWindowCallback( hwnd, windows):
-        temp = []
-        temp.append(hex(hwnd))
+
+def enum_windows():
+    def _enum_window_callback(hwnd, windows):
         left, top, right, bottom = win32gui.GetWindowRect(hwnd)
         if right > 0 and bottom > 0 and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
-            temp.append(win32gui.GetClassName(hwnd))
-            temp.append(win32gui.GetWindowText(hwnd))
-            temp.append( (left, top, right, bottom) )
-            windows[hwnd] = temp
+            class_name = win32gui.GetClassName(hwnd)
+            window_text = win32gui.GetWindowText(hwnd)
+            window = {
+                'ClassName': class_name,
+                'WindowText': window_text,
+                'Coordinates': (left, top, right, bottom),
+                'HWND': hwnd
+            }
+            windows[class_name] = window
 
     windows = {}
-    win32gui.EnumWindows(_EnumWindowCallback, windows)
-    print('total window with %d classes', (len(windows)))
+    win32gui.enum_windows(_enum_window_callback, windows)
+    print('total window with {} classes'.format(len(windows)))
 
-    for item in windows:
-        app = windows[item]
-        print(app)
-        if app[2] == config['app']:
-            return item, app
+    if windows.get(config['app']):
+        return windows['HWND'], windows
+    return None
 
-def getImage():
+
+def get_image():
     hwnd, app = EnumWindows()
     print('start run')
-    rect = app[3]
+    rect = app['Coordinates']
 
     dc = win32gui.CreateDC('DISPLAY', None, None)
 
@@ -52,33 +53,34 @@ def getImage():
     user32 = ctypes.windll.LoadLibrary("C:\\Windows\\System32\\user32.dll")
     hwindc = win32gui.GetWindowDC(hwnd)
     srcdc = win32ui.CreateDCFromHandle(hwindc)  #用dc来获取全屏的截图
-    
-    memdc = srcdc.CreateCompatibleDC()                                             
-    
-    bmp = win32ui.CreateBitmap()    
 
-    bmp.CreateCompatibleBitmap(srcdc, width, height)    
-    memdc.SelectObject(bmp)         
-    
-    memdc.BitBlt((0, 0), (width, height), srcdc, (0, 0), win32con.SRCCOPY) 
-    # 
+    memdc = srcdc.CreateCompatibleDC()
+
+    bmp = win32ui.CreateBitmap()
+
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+
+    memdc.BitBlt((0, 0), (width, height), srcdc, (0, 0), win32con.SRCCOPY)
+    #
     #user32.PrintWindow(hwnd, memdc.GetSafeHdc(), 0)
     bmpinfo = bmp.GetInfo()
     bmpInt = bmp.GetBitmapBits(False)
 
     return bmpInt, width, height
 
-def doWork(image, work):
+
+def do_work(image, work):
     print(work)
 
-   
+
 if __name__ == '__main__':
     #multiprocessing.Process(target = run)
-    img, width, height = getImage()
+    img, width, height = get_image()
     img = np.array(img, dtype=np.uint8).reshape(height, width, 4)
 
     for work in config['work']:
-        doWork(img, work)
+        do_work(img, work)
 
     cv2.imshow('image', img)
     cv2.waitKey(0)
